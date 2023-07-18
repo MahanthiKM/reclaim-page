da31aeda10969743c51832f1bee349a4032441c5
Ahmed Amin <ahmed.amin.abd.elwadod@gmail.com>
2023-07-18 18:32:09 +0200

    revert section 1

--- site/templates/default.php
+++ site/templates/default.php
@@ -29,14 +29,7 @@
             </ul>
         </nav>
     </header>
-    <div class="section-one">
-        <h1 class="main-title"><?= $page->title() ?></h1>
-        <img class="reclaim-logo" src="<?= $page->image('reclaim-logo.png')->url() ?>" alt="Reclaim Logo">
-        <div class="paragraph">
-            <p><?= $page->text() ?></p>
-        </div>
-        <button class="learn-more">LEARN MORE</button>
-    </div>
+
 
 
 </body>
